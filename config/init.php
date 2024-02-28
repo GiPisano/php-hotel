31ebf5e3928633d2c1ff5f93724f4d1831a57bc1
GiPisano <pisanogiacomo9@gmail.com>
2024-02-28 10:29:39 +0100

    add true false init.php

--- config/init.php
+++ config/init.php
@@ -2,9 +2,11 @@
 
     require_once __DIR__ . "/../data/hotels.php";
 
-    $filter_parking = isset($_GET['parking']) ?? 'off';
-    $filter_vote = $_GET['vote'] ?? 'false';
+    $filter_parking = isset($_GET['parking']) ? true : false;
 
-    if($filter_parking) $hotels = array_filter($hotels, fn($hotel) => $hotel['parking']);
+    $filter_vote = $_GET['vote'] ?? false;
 
-    if($filter_vote) $hotels = array_filter($hotels, fn($hotel) => $hotel['vote'] >= $filter_vote);
+        if($filter_parking) $hotels = array_filter($hotels, fn($hotel) => $hotel['parking']);
+    
+        if($filter_vote) $hotels = array_filter($hotels, fn($hotel) => $hotel['vote'] >= $filter_vote);
+
